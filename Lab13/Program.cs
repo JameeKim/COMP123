cc0ae74110b3878b065badb5e38efc2ea56c7366
Jamee Kim <jameekim@nodaplife.me>
2020-06-26 12:32:08 -0400

    Week 5 lab
    Co-authored-by: Pablo <p.s.g2000@hotmail.com>
    Co-authored-by: Md Shahzaman Parvej <mparvej1@my.centennialcollege.ca>
    Co-authored-by: Thedyson Eduard Luzon <tluzon1@my.centennialcollege.ca>
    Co-authored-by: raphael.puente@gmail.com <raphael.puente@gmail.com>

---
+++ Lab13/Program.cs
@@ -0,0 +1,172 @@
+﻿/**
+ * COMP123 Section 006 - Summer 2020
+ * Lab 13 (Fri Jun 26, 2020): Course
+ */
+
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace COMP123.Lab13
+{
+    public enum DisplayOption
+    {
+        All,
+        Code,
+        Name,
+        Prerequisite,
+        Semester,
+    }
+
+    class Course
+    {
+        public string Code { get; }
+        public string Description { get; }
+        public string Name { get; }
+        public string Prerequisites { get; }
+        public int Semester { get; }
+
+        public Course(string code, string name, string description, string semester, string prerequisites)
+        {
+            Code = code;
+            Name = name;
+            Description = description;
+            Semester = Convert.ToInt32(semester);
+            Prerequisites = prerequisites;
+        }
+
+        public override string ToString()
+        {
+            string description = Description.Length > 60 ? Description.Substring(0, 60) : Description;
+            return $"{Code}, {Name}, {Semester}, {Prerequisites}, {description}";
+        }
+    }
+
+    class CourseManager
+    {
+        private static List<Course> courses;
+
+        public static void Display(DisplayOption option, string toMatch = "")
+        {
+
+            switch (option)
+            {
+                case DisplayOption.All:
+                    foreach (Course course in courses)
+                    {
+                        Console.WriteLine(course);
+                    }
+                    break;
+
+                case DisplayOption.Code:
+                    foreach (Course course in courses)
+                    {
+                        if (toMatch == "" || course.Code == toMatch)
+                        {
+                            Console.WriteLine(course);
+                        }
+                    }
+                    break;
+
+                case DisplayOption.Name:
+                    foreach (Course course in courses)
+                    {
+                        if (toMatch == "" || course.Name == toMatch)
+                        {
+                            Console.WriteLine(course);
+                        }
+                    }
+                    break;
+
+                case DisplayOption.Prerequisite:
+                    foreach (Course course in courses)
+                    {
+                        if (toMatch == "" || course.Prerequisites.Contains(toMatch))
+                        {
+                            Console.WriteLine(course);
+                        }
+                    }
+                    break;
+
+                case DisplayOption.Semester:
+                    foreach (Course course in courses)
+                    {
+                        if (toMatch == "" || course.Semester.ToString() == toMatch)
+                        {
+                            Console.WriteLine(course);
+                        }
+                    }
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException("option", option, "not a valid value for DisplayOption");
+            }
+        }
+
+        public static void WriteCourses(string filename)
+        {
+            courses = new List<Course>() {
+                new Course("COMP123", "Programming 2", "Level up your C# skills to a new level", "3", "COMP100"),
+                new Course("COMP100", "Programming 1", "Introduction to C#", "2", "")
+            };
+
+            TextWriter writer = new StreamWriter(filename);
+            foreach (Course course in courses)
+            {
+                writer.WriteLine($"{course.Code};{course.Name};{course.Semester};{course.Prerequisites};{course.Description}");
+            }
+            writer.Close();
+        }
+
+        public static void LoadCourses(string filename)
+        {
+            courses = new List<Course>();
+
+            TextReader reader = new StreamReader(filename);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                string[] data = line.Split(';');
+                courses.Add(new Course(data[0], data[1], data[4], data[2], data[3]));
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "./Courses.txt";
+            try
+            {
+                CourseManager.LoadCourses(filename);
+
+                Console.WriteLine("\nAll courses");
+                CourseManager.Display(DisplayOption.All);
+
+                string toSearch = "COMP100";
+                Console.WriteLine("\nWith prerequsite " + toSearch);
+                CourseManager.Display(DisplayOption.Prerequisite, toSearch);
+
+                Console.WriteLine("\nWith title " + toSearch);
+                CourseManager.Display(DisplayOption.Code, toSearch);
+
+                toSearch = "Programing 1";
+                Console.WriteLine("\nWith name " + toSearch);
+                CourseManager.Display(DisplayOption.Name, toSearch);
+
+                toSearch = "2";
+                Console.WriteLine("\nIn semester " + toSearch);
+                CourseManager.Display(DisplayOption.Semester, toSearch);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Given file not found; creating one...");
+                CourseManager.WriteCourses(filename);
+                Console.WriteLine($"File written to {filename}. Run the command again.");
+            }
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,148 @@
+﻿/**
+ * COMP123 Section 006 - Summer 2020
+ * Lab 17 (Fri Jun 26, 2020): Exception
+ */
+
+using System;
+
+namespace COMP123.Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PreventAppCrash(DivisionNoHandling);
+            //DivisionNoHandling();
+            //DivisionWithExceptionHandling();
+        }
+
+        delegate void SomeMethod();
+
+        /// <summary>Catches the thrown error just for convenience</summary>
+        static void PreventAppCrash(SomeMethod method)
+        {
+            try
+            {
+                method.Invoke();
+                Console.WriteLine("ERROR: The method did not throw an exception");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception successfully thrown: {e}");
+            }
+        }
+
+        /// <summary>Throws a "Division by Zero" exception</summary>
+        static int Division(int top, int bottom) => top / bottom;
+
+        static void DivisionNoHandling()
+        {
+            Division(1, 0);
+        }
+
+        static void DivisionWithExceptionHandling()
+        {
+            try
+            {
+                Division(1, 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Divide by Zero Error: {e}");
+            }
+        }
+
+        static void DivisionWithExceptionHandlingMoreInfo()
+        {
+            //call the Division method with argument 1 and 0
+            //You will catch the exception and print the associated message
+            //your application should not crash
+            try
+            {
+                Division(1, 0);
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+
+        static void DivisionWithExceptionHandlingThrow()
+        {
+            //call the Division method with argument 1 and 0
+            //You will catch the exception and print the associated message
+            //You should create and throw a new exception
+            //your application will crash
+            try
+            {
+                Division(1, 0);
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+            throw new Exception("Greetings");
+
+
+        }
+
+        static void GeneratingException()
+        {
+            // new Exception("Hi");//You should create and throw a new exception
+            throw new Exception("Hi");//your application will crash
+
+        }
+
+        /* Create a class that inherits from Exception class with the following specification:
+        Should have the text “Exception” in its name
+        Should inherits form Exception
+        Should have a constructor that takes a single string argument.This constructor should invoke the base class constructor with the argument
+        */
+
+        public class MyException : Exception
+        {
+            public MyException(string message) : base(message)
+            {
+            }
+        }
+
+        static void GeneratingCustomException()//this one
+        {
+            //You should create and throw an object of your new exception class
+            //your application will crash
+            // throw it!! :D yay
+            throw new MyException("My custom exception");
+        }
+
+        static void HandlingRandomException()
+        {
+            //You should call the method below and catch all the possible exceptions individually and display a sensible output
+            //your application will not crash
+            try
+            {
+                Console.WriteLine("Trying method below");
+                GeneratingRandomException();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void GeneratingRandomException()
+        {
+            int exceptionType = new Random().Next() % 6;
+            switch (exceptionType)
+            {
+                case 0:
+                    throw new IndexOutOfRangeException();
+                case 1:
+                    throw new NullReferenceException();
+                case 2:
+                    throw new InvalidOperationException();
+                case 3:
+                    throw new ArithmeticException();
+                case 4:
+                    throw new FormatException();
+                case 5:
+                    throw new InvalidCastException();
+                case 6:
+                    throw new OutOfMemoryException();
+            }
+        }
+    }
+}
